d9468bcda416897e6c2052cb0f8884a78fc5c4bf
Joe Jackson <joejackson@macbook-pro-9.home>
2024-09-15 16:06:23 +0100

    remove unused app module files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CommsModule } from './comms/comms.module';
+import { UserModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CommsModule, UserModule],
 })
 export class AppModule {}
