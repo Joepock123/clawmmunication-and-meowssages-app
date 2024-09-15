2a7de47326dbcbb3d0a5376331be46862b22642e
Joe Jackson <joejackson@macbook-pro-9.home>
2024-09-15 18:19:21 +0100

    add cors and change port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.use(
+    cors({
+      origin: 'http://localhost:3000',
+    }),
+  );
+
+  await app.listen(3001);
 }
 bootstrap();
